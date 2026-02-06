a60d91e694e4404981e9e73c288eacb48f5e599e
Toby <tobydeh@gmail.com>
2026-02-06 09:24:40 +0000

    chore(showcase): disable diagnostics config

--- showcase/main.go
+++ showcase/main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	app := drift.NewApp(App())
-	app.Diagnostics = &engine.DiagnosticsConfig{
-		ShowFPS:          false,
-		ShowFrameGraph:   false,
-		TargetFrameTime:  16667 * time.Microsecond, // ~16.67ms for 60fps
-		ShowLayoutBounds: false,
-		DebugServerPort:  9999,
-	}
+	// app.Diagnostics = &engine.DiagnosticsConfig{
+	// 	ShowFPS:          false,
+	// 	ShowFrameGraph:   false,
+	// 	TargetFrameTime:  16667 * time.Microsecond, // ~16.67ms for 60fps
+	// 	ShowLayoutBounds: false,
+	// 	DebugServerPort:  9999,
+	// }
 	app.Run()
 }
